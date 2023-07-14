6b627845073a0fe21ffd1bf243eb14149a402728
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-07-14 14:16:02 +0900

    thymeleaf practice

--- ch2/src/main/java/com/fastcampus/ch2/HomeController.java
+++ ch2/src/main/java/com/fastcampus/ch2/HomeController.java
@@ -2,8 +2,15 @@ package com.fastcampus.ch2;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 // 1. 원격 프로그램으로 등록
 @Controller
@@ -12,6 +19,25 @@ public class HomeController { // 원격 프로그램
     @RequestMapping("/")
     public String main() {
         return "index"; // templates/index.html
+    }
+
+    @GetMapping("/test")
+    public String test(Model model, HttpServletRequest request) {
+        request.setAttribute("year", 2022); // request 객체에 저장
+
+        HttpSession session = request.getSession(); // 요청으로부터 session 객체를 얻어온다.
+        session.setAttribute("id", "asdf"); // session객체에 id를 저장
+
+        ServletContext application = session.getServletContext();
+        application.setAttribute("email", "jyeon@aaa.com");
+
+        model.addAttribute("lastName", "Jyeon");
+        model.addAttribute("firstName", "C");
+        model.addAttribute("list", Arrays.asList("aaa","bbb","ccc","ddd","eee"));
+        model.addAttribute("bno", "123");
+        model.addAttribute("user", new User("aaa",20));
+
+        return "test"; // templates/test.html
     }
 }
 
--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URLEncoder;
 
 @Controller
@@ -23,17 +25,24 @@ public class LoginController {
     //    @RequestMapping(value = "/login/login", method = {RequestMethod.POST,RequestMethod.GET})
     //    @RequestMapping(value = "/login/login", method = RequestMethod.POST) // 아래와 같다.
     @PostMapping("/login")
-    public String login(String id, String pwd, Model model) throws Exception{
+    public String login(HttpServletRequest req, String id, String pwd, RedirectAttributes model) throws Exception{
         // 1. id, pwd를 확인
         if(loginCheck(id,pwd)) {
             // 2. 일치하면, userInfo.html
             model.addAttribute("id", id);
             model.addAttribute("pwd", pwd);
+
             return "userInfo"; // userInfo.html
         } else {
             // 일치하지 않으면, login.html로 이동
-            String msg = URLEncoder.encode("id 또는 패스워드가 일치하지 않습니다.", "utf-8");
-            return "redirect:/login/login?msg=" + msg; // redirect는 GET요쳥 -> GetMapping("/login") 받음
+//            String msg = URLEncoder.encode("id 또는 패스워드가 일치하지 않습니다.", "utf-8");
+            String msg ="id 또는 패스워드가 일치하지 않습니다.";
+            model.addAttribute("msg", msg);
+            model.addFlashAttribute("msg","일회용 메시지");
+            req.setAttribute("msg", "request에 저장된 msg");
+
+            return "redirect:/";
+            //            return "redirect:/login/login?msg=" + msg; // redirect는 GET요쳥 -> GetMapping("/login") 받음
         }
     }
 
