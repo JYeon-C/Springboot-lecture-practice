79cb58f1bf18e60bb8ae2b955eae172a1c93d9fd
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-07-30 21:20:45 +0900

    ch3 05. commit

--- ch3/src/main/java/com/fastcampus/ch3/di3/AppConfig.java
+++ ch3/src/main/java/com/fastcampus/ch3/di3/AppConfig.java
@@ -1,18 +1,20 @@
 package com.fastcampus.ch3.di3;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
-    @Bean
-    Car car() {return new Car();}
-
-    @Bean
-    @Scope("prototype")
-    Engine engine() {return new Engine();}
-
-    @Bean
-    Door door() {return new Door();}
+//    @Bean
+//    Car car() {return new Car();}
+//
+//    @Bean
+//    @Scope("prototype")
+//    Engine engine() {return new Engine();}
+//
+//    @Bean
+//    Door door() {return new Door();}
 }
