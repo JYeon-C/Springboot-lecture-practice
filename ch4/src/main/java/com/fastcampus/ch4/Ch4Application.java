beb97c4d5efae9c6bb733b88dd42496581d8fa24
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-07-31 21:47:25 +0900

    ch4 01. commit

--- ch4/src/main/java/com/fastcampus/ch4/Ch4Application.java
+++ ch4/src/main/java/com/fastcampus/ch4/Ch4Application.java
@@ -1,13 +1,28 @@
 package com.fastcampus.ch4;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManagerFactory;
+
+
 @SpringBootApplication
-public class Ch4Application {
+public class Ch4Application implements CommandLineRunner {
+
+    @Autowired
+    EntityManagerFactory emf;
 
     public static void main(String[] args) {
-        SpringApplication.run(Ch4Application.class, args);
+        SpringApplication app = new SpringApplication(Ch4Application.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("emf = " + emf);
+    }
 }
