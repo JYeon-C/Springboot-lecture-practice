fd8d55ef94dac31be3e13d4b6bdd6497995f9b5e
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-08-01 01:02:22 +0900

    ch4 03. commit

--- ch4/src/main/java/com/fastcampus/ch4/Ch4Application.java
+++ ch4/src/main/java/com/fastcampus/ch4/Ch4Application.java
@@ -6,7 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import java.util.Date;
 
 
 @SpringBootApplication
@@ -23,6 +26,36 @@ public class Ch4Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("emf = " + emf);
+        EntityManager em = emf.createEntityManager();
+        System.out.println("em = " + em);
+        EntityTransaction tx = em.getTransaction();
+
+        User user = new User();
+        user.setId("aaa");
+        user.setPassword("1234");
+        user.setName("choi");
+        user.setEmail("aaa@aaa.com");
+        user.setInDate(new Date());
+        user.setUpDate(new Date());
+
+        tx.begin(); // 트랜잭션 시작
+        // 1. 저장
+        em.persist(user); // user엔티티를 em에 영속화(저장)
+
+        // 2. 변경
+        user.setPassword("4321");
+        tx.commit(); // 트랜잭션 종료
+
+        // 3. 조회
+        User user2 = em.find(User.class, "aaa");
+        System.out.println("(user == user2) = " + (user == user2));
+
+        User user3 = em.find(User.class, "bbb");
+        System.out.println("(user == user3) = " + (user == user3));
+
+        // 4. 삭제
+        tx.begin(); // 트랜잭션 시작
+        em.remove(user);
+        tx.commit(); // 트랜잭션 종료
     }
 }
